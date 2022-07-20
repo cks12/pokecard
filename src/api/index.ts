b80e173afbc46b28f83a3f264c3a1747df88b195
Caio Fresneda <caiofresneda@gmail.com>
2022-07-20 20:38:27 -0300

    created api

---
+++ src/api/index.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosInstance } from "axios";
+import { pokemonInterface, pokemonListInterface, pokemonSpecie } from "./pokemonInterface";
+
+export interface pokemonInfosProps {
+    pokemonInformation:pokemonInterface,
+    pokemonSpecies:pokemonSpecie 
+}
+
+
+class pokemon {
+    baseUrl: string;
+    api: AxiosInstance;
+    pokemonIndex: number;
+    page: number
+    constructor(){
+        this.baseUrl = 'https://pokeapi.co/api/v2';
+        this.api = axios.create({
+            baseURL:this.baseUrl
+        });
+        this.pokemonIndex = 0;
+        this.page = 0
+    }
+    async getPokemon(id: string | number): Promise<pokemonInfosProps> {
+        const pokemon = await this.api.get(`/pokemon/${id}`);
+        const pokemonSpecies = await this.api.get(`/pokemon-species/${id}/`);
+        return {
+            pokemonInformation:pokemon.data,
+            pokemonSpecies:pokemonSpecies.data,
+        }
+    }
+
+    async listPokemon(page: number = 1): Promise<pokemonListInterface>{
+        console.log("loading...")
+        const pokemon = await this.api.get(`/pokemon?offset=${page*20}&limit=20`)
+        console.log(pokemon)
+        return pokemon.data
+    }
+}
+
+export default new pokemon()
---
+++ src/api/pokemonInterface.d.ts
@@ -0,0 +1,112 @@
+interface versionBase {
+    name: string,
+    url: string,
+}
+
+export interface abilitie {
+    ability: versionBase,
+    is_hidden: boolean,
+    slot: number
+}
+
+export interface forms {
+    name: string,
+    url: string,
+}
+
+export interface game_indices {
+    game_index: number,
+
+}
+
+export interface version_details {
+    rarity: number,
+    version:versionBase,
+}
+
+export interface held_items {
+    item: {
+        name: string,
+        url: string
+      }
+      version_details: version_details[]
+}
+
+export interface sprites {
+    back_default: null | string,
+    back_female: null | string,
+    back_shiny: null | string,
+    back_shiny_female: null | string,
+    front_default: null | string,
+    front_female: null | string,
+    front_shiny: null | string,
+    other: {
+      dream_world: {
+        front_default: null | string,
+        front_female: null | string,
+      },
+      home: {
+        front_default: null | string,
+        front_female: null | string,
+        front_shiny: null | string,
+        front_shiny_female: null | string,
+      },
+      'official-artwork': {
+        front_default: null | string,
+      }
+    },
+}
+
+export interface stats {
+    base_stat: number,
+    effort: number,
+    stat: versionBase
+}
+
+export interface types {
+    "slot": number,
+    "type": versionBase
+}
+
+export interface pokemonInterface {
+    abilities: abilitie[],
+    base_experience: number,
+    forms:forms[],
+    game_indices: game_indices[],
+    height: number,
+    held_items: held_items[],
+    id: number,
+    is_default: boolean,
+    location_area_encounters:string,
+    name: string,
+    order: number,
+    species: versionBase,
+    sprites:sprites,
+    stats: stats[],
+    types: types[]
+}
+
+export interface pokemonListInterface {
+    count: number,
+    next: null | string,
+    previous: null | string,
+    results:versionBase[]
+}
+
+
+export interface flavor_text_entries {
+    "flavor_text": string
+    "language": versionBase
+}
+
+export interface pokemonSpecie {
+    "base_happiness": number,
+    "capture_rate": number,
+    "color": versionBase,
+    "egg_groups": versionBase[],
+    "evolution_chain": {
+        "url": string
+    },
+    "evolves_from_species": null,
+    "flavor_text_entries": flavor_text_entries[] 
+}
